0ce9dd70ba0870915d0c609a4ca7b4072e8dc405
Bhavy <84842331+datsBhavy@users.noreply.github.com>
2024-06-28 17:04:37 +0530

    weather code added

--- index.js
+++ index.js
@@ -1,12 +1,74 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+// const http = require('http');
+// const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node from github actions!\n'
-  res.end(msg);
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   const msg = 'Hello Node from github actions!\n'
+//   res.end(msg);
+// });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+// server.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}/`);
+// });
+
+
+const api = require('./api.json');
+const axios = require('axios');
+
+const getWeather = async location => {
+	const city = location || 'dhaka';
+	const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+		api.key
+	}`;
+
+	try {
+		const response = await axios.get(url);
+
+		if (response.status === 200) {
+			try {
+				if (response.data.name) {
+					return response.data;
+				} else {
+					const queryError = new Error(`The location ${city} was not found`);
+					printError(queryError);
+				}
+			} catch (error) {
+				printError(error);
+			}
+		} else {
+			const statusCodeError = new Error(
+				`There was an error getting the message for ${city}(StatusCode ${
+					response.status
+				})`
+			);
+			printError(statusCodeError);
+		}
+	} catch (error) {
+		printError(error);
+	}
+};
+
+if (require.main == module) {
+	const argument = process.argv.slice(3).join(' ');
+
+	getWeather(argument).then(val => {
+		printWeather(val);
+	});
+}
+
+function printWeather(weather) {
+	let message = `Current Temperature in ${weather.name} is ${
+		weather.main.temp
+	}°C`;
+
+	console.log(message);
+}
+
+function printError(error) {
+	console.error(error.message);
+}
+
+module.exports = {
+	getWeather,
+	printWeather
+};
